f2e0dbeb0901038ff0013d6437dc4dcd3aab7e04
Mohd-Azeem1 <msajifdf123@gmail.com>
2024-06-14 20:24:23 +0530

    TestCorsError

--- src/backend/backend.js
+++ src/backend/backend.js
@@ -1,7 +1,7 @@
 
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Origin', 'purascent.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', true);
